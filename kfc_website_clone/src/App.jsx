f9f1f63d1940db852ebe42c4705928ce69fb379b
Aman Verma <vermaman3@gmail.com>
2022-06-18 01:12:34 +0530

    changes

--- kfc_website_clone/src/App.jsx
+++ kfc_website_clone/src/App.jsx
@@ -3,18 +3,40 @@ import logo from './logo.svg'
 import Navbar from './Copmonents/Navbar'
 import './App.css'
 import Footer from './Copmonents/Footer'
+import KfcDeal from './Pages/KfcDeal'
+
+import Login from './pages/Login';
+import Menu from './pages/Menu';
+import Home from './Pages/Home'
+
+import { Route, Routes, useLocation } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-    <h2>HELLO</h2>
-    <Navbar/>
-    <Footer/>
-     
-    </div>
-  )
-}
-
-export default App
+  const [loading,setLoading]=useState(true)
+  const spinner=document.getElementById("spinner")
+  const location=useLocation()
+  if(spinner){
+    setTimeout(()=>{
+      spinner.style.display="none";
+      setLoading(false)
+  },2500)
+  }
+    return (
+      !loading && (
+        <div className="App">
+          {location.pathname==="/Login" ? null : <Navbar />}
+           {/* <Navbar /> */}
+           <Routes>
+              {/* <Route path='/' element={<Home/>}/> */}
+              <Route path='/Login' element={<Login/>}/>
+              <Route path='/menu' element={<Menu/>}/> 
+              <Route path='/deals' element={<KfcDeal/>}/>       
+           </Routes>
+           {location.pathname==="/Login" ? null : <Footer />}
+        </div>
+      )
+    );
+  }
+  
+  export default App;
+  
